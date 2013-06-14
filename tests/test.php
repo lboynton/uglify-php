91cd0bdd72c63480e80df2fe1c2612d5b3f3b986
Mark Wales <mark@smallhadroncollider.com>
2013-06-14 12:59:04 +0100

    Added slightly better tests. Still not great.

--- tests/test.php
+++ tests/test.php
@@ -5,6 +5,22 @@ use UglifyPHP\CSS;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-// Check the classes are working
-echo JS::installed();
-echo CSS::installed();
+if (JS::installed()) {
+    $uglify = new JS(array('js-files/jquery.js', 'js-files/bootstrap.js', 'js-files/script.js'));
+
+    if ($uglify->minify('min.js')) {
+        echo '<p>JS compressed</p>';
+    } else {
+        echo '<p>JS error</p>';
+    }
+}
+
+if (CSS::installed()) {
+    $uglify = new CSS(array('css-files/bootstrap-responsive.css', 'css-files/bootstrap.css', 'css-files/css.css'));
+
+    if ($uglify->minify('min.css', array('max-line-len' => '80'))) {
+        echo '<p>CSS compressed</p>';
+    } else {
+        echo '<p>CSS error</p>';
+    }
+}
